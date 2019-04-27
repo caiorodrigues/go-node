3c62b2866c7ff9297e7edc279798d16f3447edd2
Caio Rodrigues <caiorodrigues@hotmail.com>
2019-04-26 21:04:47 -0300

    Commit - Rendering Data in View

---
+++ ambiente-e-conceitos/renderizando-dados-na-view/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const nunjucks = require("nunjucks");
+
+const app = express();
+
+nunjucks.configure("views", {
+  autoescape: true,
+  express: app,
+  watch: true
+});
+
+app.set("view engine", "njk");
+
+const users = ["Diego Fernandes", "Robson Marques", "Cleiton Souza"];
+
+app.get("/", (req, res) => {
+  return res.render("list", {
+    users
+  });
+});
+
+app.get("/new", (req, res) => {
+  return res.render("new");
+});
+
+app.listen(3000);
